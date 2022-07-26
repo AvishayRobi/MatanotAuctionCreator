5ad2cf2be0a036632ef9039a4487f34a52295bfd
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-26 11:50:26 +0300

    Create MatanotOrder.cs

---
+++ Model/MatanotOrder.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using WallaShops.Utils;
+using WSOrderCreator.Model;
+
+namespace MatanotAuctionCreator.Model
+{
+  public class MatanotOrder : ICasualOrderConvertible
+  {
+    [JsonProperty("order_id")]
+    public int OrderID { get; set; }
+
+    [JsonProperty("total_amount")]
+    public int TotalPrice { get; set; }
+
+    [JsonProperty("order_date")]
+    public string OrderDate { get; set; }
+
+    [JsonProperty("client_info")]
+    public ClientInfo ClientInfo { get; set; }
+
+    [JsonProperty("shipping_info")]
+    public ShippingInfo ShipmentInfo { get; set; }
+
+    [JsonProperty("products_info")]
+    public List<MatanotOrderItem> Items { get; set; }
+
+    public CasualOrder ConvertToCasualOrder()
+      =>
+      new CasualOrder()
+      {
+        TotalOrderPrice = this.TotalPrice,
+        AffiliateName = string.Empty,
+        DateCreated = this.OrderDate,
+        Items = getConvertedItems(),
+        EntryID = this.OrderID,
+        ShopID = getShopID(),
+        ShippingDetails = new ShippingDetails()
+        {
+          ZipCode = int.Parse(this.ShipmentInfo.ZipCode),
+          StreetNumber = this.ShipmentInfo.StreetNumber,
+          StreetName = this.ShipmentInfo.StreetName,
+          City = this.ShipmentInfo.City,
+          Enterance = string.Empty,
+          Apratment = 0,
+          Floor = 0
+        },
+        ShopperDetails = new ShopperDetails()
+        {
+          Idz = WSGeneralUtils.GetAppSettings("DefaultShopperIdz"),
+          FirstName = this.ClientInfo.FirstName,
+          Email = this.ClientInfo.EmailAddress,
+          LastName = this.ClientInfo.LastName,
+          PhoneNumber = getPhoneNumber(),
+          Birthdate = DateTime.Today
+        }
+        .ValidateIdz()
+      };
+
+    private string getPhoneNumber()
+      =>
+      string.IsNullOrEmpty(this.ClientInfo.PhoneNumber)
+      ? WSGeneralUtils.GetAppSettings("DefaultShopperPhoneNumber")
+      : this.ClientInfo.PhoneNumber;
+
+    private int getShopID()
+      =>
+      this.Items
+      .First()
+      .ShopID;
+
+    private List<OrderItem> getConvertedItems()
+      =>
+      this.Items
+      .ConvertAll(generateItem);
+
+    private OrderItem generateItem(MatanotOrderItem productInfo)
+      =>
+      new OrderItem()
+      {
+        AuctionID = int.TryParse(productInfo.SKU, out int x) ? int.Parse(productInfo.SKU) : 0,
+        PriceBeforeDiscount = productInfo.Price,
+        CouponAmount = productInfo.WallaPrice,
+        Quantity = productInfo.Quantity,
+        Description = productInfo.Name,
+        FinalPrice = productInfo.Price,
+        ShopID = productInfo.ShopID
+      };
+  }
+}
