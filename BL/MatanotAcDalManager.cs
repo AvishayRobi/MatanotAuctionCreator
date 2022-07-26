7eccb7d253396d38b9581e79d1618edafd97ce56
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-26 11:33:33 +0300

    Create MatanotAcDalManager.cs

---
+++ BL/MatanotAcDalManager.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using MatanotAuctionCreator.DAL;
+using MatanotAuctionCreator.Model;
+using WallaShops.Utils;
+
+namespace MatanotAuctionCreator.BL
+{
+  public class MatanotAcDalManager
+  {
+    #region Data Members
+    private MatanotAcDal dal { get; }
+    #endregion
+
+    #region Ctor
+    public MatanotAcDalManager()
+    {
+      this.dal = new MatanotAcDal();
+    }
+    #endregion
+
+    public IEnumerable<MatanotOrder> GetPreviousUnprocessedOrders()
+    {
+      DataTable dt = this.dal.GetUnprocessedAuctions();
+
+      return from DataRow dr
+             in dt.Rows
+             select new MatanotOrder()
+             {
+               OrderDate = WSStringUtils.ToString(dr["order_date"]),
+               TotalPrice = WSStringUtils.ToInt(dr["total_price"]),
+               OrderID = WSStringUtils.ToInt(dr["order_id"]),
+               ClientInfo = new ClientInfo()
+               {
+                 FirstName = WSStringUtils.ToString(dr["client_first_name"]),
+                 EmailAddress = WSStringUtils.ToString(dr["client_email"]),
+                 LastName = WSStringUtils.ToString(dr["client_last_name"]),
+                 PhoneNumber = WSStringUtils.ToString(dr["client_phone"]),
+                 WorkerID = WSStringUtils.ToString(dr["worker_id"])
+               },
+               ShipmentInfo = new ShippingInfo()
+               {
+                 StreetNumber = WSStringUtils.ToString(dr["shipping_street_num"]),
+                 StreetName = WSStringUtils.ToString(dr["shipping_street"]),
+                 City = WSStringUtils.ToString(dr["shipping_city"]),
+                 ZipCode = WSStringUtils.ToString(dr["zip_code"])
+               },
+               Items = getItems(WSStringUtils.ToInt(dr["order_id"])).ToList()
+             };
+    }
+
+    public void AttachFileToOrders(IEnumerable<MatanotOrder> orders, string virtualPath)
+      =>
+      orders
+      .Select(getOrderID)
+      .ApplyEach(i => attachFileToSingleOrder(i, virtualPath));
+
+    public void BurnOrders(IEnumerable<MatanotOrder> orders)
+      =>
+      orders
+      .ApplyEach(burnOrder);
+
+    public void UpdateOrderStatus(int orderID, eOrderStatus orderStatus, string failureReason = "")
+      =>
+      this.dal
+      .UpdateOrderStatus(orderID, (int)orderStatus, failureReason);
+
+    private void burnOrder(MatanotOrder order)
+    {
+      this.dal
+        .BurnOrder(order);
+
+      order
+        .Items
+        .ForEach(i => burnOrderItem(i, order.OrderID));
+    }
+
+    private IEnumerable<MatanotOrderItem> getItems(int orderID)
+    {
+      DataTable dt = this.dal.GetUnprocessedAuctionItems(orderID);
+
+      return from DataRow dr
+             in dt.Rows
+             select new MatanotOrderItem()
+             {
+               WallaPrice = WSStringUtils.ToInt(dr["walla_price"]),
+               Name = WSStringUtils.ToString(dr["product_name"]),
+               Quantity = WSStringUtils.ToInt(dr["quantity"]),
+               ShopID = WSStringUtils.ToInt(dr["shop_id"]),
+               Price = WSStringUtils.ToInt(dr["price"]),
+               SKU = WSStringUtils.ToString(dr["sku"])
+             };
+    }
+
+    private void burnOrderItem(MatanotOrderItem item, int orderID)
+      =>
+      this.dal
+      .BurnOrderItem(item, orderID);
+
+    private void attachFileToSingleOrder(int orderID, string virtualPath)
+      =>
+      this.dal
+      .AttachFileToOrders(orderID, virtualPath);
+
+    private int getOrderID(MatanotOrder order)
+      =>
+      order.OrderID;
+  }
+}
