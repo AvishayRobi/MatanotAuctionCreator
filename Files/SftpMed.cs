7c68e8d802fc25bea66eeff5889ab71f7d0d5ec7
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-31 11:56:49 +0300

    Create SftpMed.cs

---
+++ Files/SftpMed.cs
@@ -0,0 +1,69 @@
+using System.Linq;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using WallaShops.Utils;
+
+namespace MatanotAuctionCreator.Files
+{
+  public class SftpMed
+  {
+    #region Data Members
+    private SftpClient client { get; }
+    private string folderPath { get; }
+    #endregion
+
+    #region Ctor Dtor
+    public SftpMed()
+    {
+      this.folderPath = WSGeneralUtils.GetAppSettings("---");
+      string userName = WSGeneralUtils.GetAppSettings("---");
+      string password = WSGeneralUtils.GetAppSettings("---");
+      string host = WSGeneralUtils.GetAppSettings("---");
+
+      this.client = new SftpClient(host, userName, password);
+      this.client.Connect();
+    }
+
+    ~SftpMed()
+    {
+      this.client.Disconnect();
+      this.client.Dispose();
+    }
+    #endregion
+
+    public string GetFirstFileName()
+    {
+      this.client
+        .ChangeDirectory("/");
+
+      return this.client
+        .ListDirectory(this.folderPath)
+        .Select(getFileName)
+        .Where(isFileTypeJson)
+        .FirstOrDefault()
+        .LastAppearanceAfter('/');
+    }
+
+    public string ReadFile(string fileName)
+    {
+      this.client
+        .ChangeDirectory(this.folderPath);
+
+      return this.client
+        .ReadAllText(this.folderPath + "/" + fileName);
+    }
+
+    public void DeleteRemoteFile(string fileName)
+      =>
+      this.client
+      .DeleteFile(this.folderPath + "/" + fileName);
+
+    private string getFileName(SftpFile file)
+      =>
+      file.FullName;
+
+    private bool isFileTypeJson(string fileName)
+      =>
+      fileName.Contains("json");
+  }
+}
